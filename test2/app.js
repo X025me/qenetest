5b3c63e2eb506501a854ad956234f284e013edee
X025me <exo25me@gmail.com>
2023-10-13 13:23:32 +0300

    data

--- test2/app.js
+++ test2/app.js
@@ -1,29 +1,28 @@
 const express = require('express')
 const fs = require('fs')
-var bodyParser = require('body-parser');
+
+const bodyParser = require('body-parser');
 
 // let ejs = require('ejs')
 
 let app  = express()
-app.set('template engine', 'ejs');
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // app.use(express.bodyParser());
 
 
-
-app.get('/', (req, res) =>{
-    res.render('index.ejs')
-})
 app.post('/text', (req, res) =>{
     let data = fs.readFileSync('read.txt', {encoding: 'utf-8'})
-    // const file = fs.readFileSync('path/to/data.txt', 'utf8');
-    // if(file.indexOf("keyword") >= 0) {
-        
-    // }
+    // // le
+    // // const file = fs.readFileSync('path/to/data.txt', 'utf8');
+    if(data.indexOf(req.body.data) >= 0) {
+        console.log('found one')
+    }
 
-    console.log(req.query)
-    res.send({})
+    console.log(req.body)
+    res.json({data: 'data'})
 })
 
 
-app.listen(3000, 'localhost')
+app.listen(3000, () =>{
+    console.log('running server')
+})
